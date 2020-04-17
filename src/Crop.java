662189589fb25ec2b8a1818bb969e0f5127aee67
Sarah Bealing <sarahbealing@Sarah-Bealing.local>
2020-04-17 14:54:26 +1200

    Crop superclass

---
+++ src/Crop.java
@@ -0,0 +1,17 @@
+public class Crop {
+
+    private double purchasingPrice;
+    private double sellingPrice;
+    private int growingRate;
+    private int thirstLevel = 0;
+
+    public Crop(double tempPurchasingPrice, double tempSellingPrice, int tempGrowingRate) {
+        purchasingPrice = tempPurchasingPrice;
+        sellingPrice = tempSellingPrice;
+        growingRate = tempGrowingRate;
+    }
+
+    public void Harvest() {
+        //TO DO: implement me
+    }
+}
