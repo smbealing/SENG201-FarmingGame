a31055d1a9145ef99cc80b8befb25c7a1fd39e98
Sarah Bealing <sarahbealing@Sarah-Bealing.local>
2020-04-17 15:06:01 +1200

    Add Corn class

--- src/Corn.java
+++ src/Corn.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class Corn extends Crop {
 
-public class Corn {
+    //purchasePrice = 0.50
+    //sellingPrice = 1.50
+    //growingRate = 8
+
+    public Corn(){
+        super(0.5, 1.5, 8);
+    }
 }
