d8987005f730a48b565e551e01b857c9362a5fd2
Inga Tokarenko <ito16@uclive.ac.nz>
2020-04-23 17:37:38 +1200

    Started on game environment

--- src/GameEnvironment/GameEnvironment.java
+++ src/GameEnvironment/GameEnvironment.java
@@ -1,5 +1,12 @@
 package GameEnvironment;
 
+import java.util.ArrayList;
+
 public class GameEnvironment {
+	
+	private GameState state;
+	private int numDays;
+	private int numMembers;
+//	private Scanner s;
 
 }
