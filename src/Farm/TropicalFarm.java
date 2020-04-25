67085f306b5860e1d02eb2a8de17398581f90cb1
Sarah Bealing <sarahbealing@Sarah-Bealing.local>
2020-04-25 12:02:28 +1200

    Farm classes now have capacity for crops and animals.

--- src/Farm/TropicalFarm.java
+++ src/Farm/TropicalFarm.java
@@ -1,14 +1,10 @@
 package Farm;
-import Crop.Banana;
-import Crop.DragonFruit;
+
 
 public class TropicalFarm extends Farm {
 
     public TropicalFarm(String tempName, Farmer tempFarmer) {
-        super(tempName, tempFarmer, "Tropical Farm");
-
-        addCrop(new Banana());
-        addCrop(new DragonFruit());
+        super(tempName, tempFarmer, "Tropical Farm", 25, 10);
 
     }
 }
