5a0f751af3e8c702d719b1fe67c39fdf13b424d0
Sarah Bealing <smb272@uc.ac.nz>
2020-05-09 14:52:13 +1200

    merge conflict in StartPanel fixed

--- src/GameEnvironment/StartPanel.java
+++ src/GameEnvironment/StartPanel.java
@@ -53,18 +53,7 @@ public class StartPanel {
 		lblFarmiza.setBounds(10, 21, 764, 155);
 		frmFarmiza.getContentPane().add(lblFarmiza);
 		
-<<<<<<< HEAD
-		JLabel lblNewLabel_1 = new JLabel("Farmiza");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Gabbaland", Font.BOLD, 60));
-		lblNewLabel_1.setBounds(10, 21, 414, 78);
-		frmFarmiza.getContentPane().add(lblNewLabel_1);
-		
-		JButton btnNewButton = new JButton("Play");
-		btnNewButton.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 20));
-		btnNewButton.setBounds(10, 172, 414, 32);
-		frmFarmiza.getContentPane().add(btnNewButton);
-=======
+
 		JButton btnPlay = new JButton("Play");
 		btnPlay.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 30));
 		btnPlay.setBounds(10, 362, 764, 64);
@@ -76,7 +65,7 @@ public class StartPanel {
 			}
 		});
 		frmFarmiza.getContentPane().add(btnPlay);
->>>>>>> ca41f85672cdb180bf78e6e92f10577cb05c1197
+
 		
 		JButton btnQuit = new JButton("Quit");
 		btnQuit.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 30));
